1cfb7575b919bc31d295c92a9c4e70ba4159e5aa
fahadssgg <fahadguzai@gmail.com>
2023-08-31 10:52:11 +0300

    twet-pro

---
+++ twitter-pro/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
